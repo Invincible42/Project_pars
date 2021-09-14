c2195f4a5c900c81e10bf39f9d355230a644fe23
Drag0m1r <545436@edu.rocmn.nl>
2021-09-14 13:50:45 +0200

    tailwind en gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+    colors:{
+      parsRed:{
+          DEFAULT: "#B82F4D",
+      },
+      parsYellow:{
+          DEFAULT: "#FFC95B",
+      },
+      parsDarkOrange:{
+        DEFAULT: "#E86A4D"
+      },
+      parsOrange:{
+        DEFAULT: "#F59C57",
+      },
+      parsPurple:{
+        DEFAULT: "#2C0540"
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- view/index.php
+++ view/index.php
@@ -1,1 +1,6 @@
-<?php?>
+<?php
+include "./header.php";
+
+
+include "./footer.php";
+?>
