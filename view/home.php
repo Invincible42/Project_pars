2e72ebb80b19255ceada95d2444e24d12d5083e6
Drag0m1r <545436@edu.rocmn.nl>
2021-09-20 21:33:31 +0200

    extra gray

--- view/home.php
+++ view/home.php
@@ -7,8 +7,8 @@
         </div>
     </div>
     <?php for($i = 0; $i < 5; $i++):?>
-    <div class="flex flex-row bg-gradient-to-tr from-gray-800 to-gray-600     rounded xl:mx-96 mx-5 my-2">
-        <div><img class="  h-20 rounded-l bg-cover" src="./view/img/nf.png"></img></div>
+    <div class="flex flex-row bg-gradient-to-t from-gray-800 to-gray-750 rounded xl:mx-96 mx-5 my-4">
+        <div><img class="h-20 rounded-l object-cover" src="./view/img/nf.png"></img></div>
         <div class="flex flex-row w-full">
             <div class="self-center justify-self-center mx-2">
                 <h2 class="text-gray-50 text-xl">My Playlist #1</h2>
