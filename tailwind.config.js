bac921d36bbbb64949c28cc7e3141c93ce58a2ae
spverroen <66566853+spverroen@users.noreply.github.com>
2021-09-21 09:42:35 +0200

    kanter u suck

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      textColor:{
+        'transparent': "transparent"
+      },
     },
     colors:{
       parsRed:{
