6c511b4cf8e755612d0e1e2256e03462f4d7eab4
Drag0m1r <545436@edu.rocmn.nl>
2021-09-21 22:44:40 +0200

    kekw

--- view/home.php
+++ view/home.php
@@ -8,7 +8,7 @@
     </div>
     <?php for($i = 0; $i < 5; $i++):?>
     <div class="flex flex-row bg-gradient-to-t from-gray-800 to-gray-750 rounded xl:mx-96 mx-5 my-4">
-        <div class="bg-parsDarkOrange w-20 rounded-bl rounded-tl"><div class="flex justify-center items-center text-transparent text-5xl bg-clip-text  bg-gradient-to-tr from-gray-900 to-gray-50 h-20 w-20 ">&#x266B;</div></div>
+        <div class="bg-parsDarkOrange w-20 rounded-bl rounded-tl"><div class="flex justify-center items-center text-transparent text-5xl bg-clip-text  bg-gradient-to-tr from-gray-900 to-gray-200 h-20 w-20 ">&#x266B;</div></div>
         <div class="flex flex-row w-full">
             <div class="self-center justify-self-center mx-2">
                 <h2 class="text-gray-50 text-xl">My Playlist #1</h2>
