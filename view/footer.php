d18151da27d6b159a61a652125a8a999de2a8ba8
Drag0m1r <545436@edu.rocmn.nl>
2021-09-22 20:17:33 +0200

    welcome text

--- view/footer.php
+++ view/footer.php
@@ -1,36 +1,36 @@
 
     <div id="footer" class="w-full bottom-0 sticky lg:relative bg-gray-800 border-t-4 border-parsOrange flex flex-row justify-around">
-        <div class="flex flex-col items-center w-1/3">
+        <div class="flex flex-col items-center w-1/4">
             <div>
                 <span><a href="?route=home" class="material-icons text-4xl text-parsOrange hover:text-parsDarkOrange m-auto my-2">home</a></span>
             </div>
             <div>
-                <span class="text-gray-50 opacity-70">Home</span>
+                <span class="text-gray-50 text-xs opacity-70">Home</span>
             </div>
 
         </div>
-        <div class="flex flex-col items-center w-1/3">
+        <div class="flex flex-col items-center w-1/4">
             <div>
                 <span><a href="?route=add-music" class="material-icons text-4xl text-parsOrange hover:text-parsDarkOrange m-auto my-2">playlist_add</a></span>
             </div>
             <div>
-                <span class="text-gray-50 opacity-70">Make Playlist</span>
+                <span class="text-gray-50 text-xs opacity-70">Create Playlist</span>
             </div> 
         </div>
-        <div class="flex flex-col items-center w-1/3">
+        <div class="flex flex-col items-center w-1/4">
             <div>
                 <span><a href="?route=add-music" class="material-icons text-4xl text-parsOrange hover:text-parsDarkOrange m-auto my-2">add</a></span>
             </div>
             <div>
-                <span class="text-gray-50 opacity-70">Add Song</span>
+                <span class="text-gray-50 text-xs opacity-70">Add Song</span>
             </div> 
         </div>
-        <div class="flex flex-col items-center w-1/3">
+        <div class="flex flex-col items-center w-1/4">
             <div>
                 <span><a href="?route=my-library" class="material-icons text-4xl text-parsOrange hover:text-parsDarkOrange m-auto my-2">library_music</a></span>
             </div>
             <div>
-                <span class="text-gray-50 opacity-70">My library</span>
+                <span class="text-gray-50 text-xs opacity-70">My library</span>
             </div>
         </div>
     </div>
--- view/home.php
+++ view/home.php
@@ -1,6 +1,17 @@
 
 <section>
     <div>
+        <div class="flex flex-col justify-center items-center mx-7 my-6">
+            <p class="text-gray-50 text-sm lg:text-2xl">
+                Welcome to <span class="font-speed text-parsOrange">PARS</span> a new music streaming platform.
+            </p>
+            <p  class="text-gray-50 text-sm lg:text-2xl">
+                Take a look around we will be launching in the next few weeks.
+            </p>
+        </div>
+        
+    </div>
+    <div>
         <div class="flex xl:mx-96 mx-5 my-4">
             <input type="text" placeholder="Search for songs or playlists...." name="search" id="" class=" focus:outline-none bg-gray-600 border-parsOrange p-1 border-2 rounded mx-auto mr-3 w-full focus:text-parsYellow">
             <button class=" rounded mx-auto"> <span class="material-icons rounded-full hover:bg-gray-800 text-parsOrange hover:text-parsDarkOrange p-1.5 text-3xl">search</span></button>
