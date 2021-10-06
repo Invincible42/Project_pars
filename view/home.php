81bcfdf544f25e9e64fae8c24b21f51860a3c922
spverroen <spverroen@gmail.com>
2021-10-06 20:10:59 +0200

    push

--- view/home.php
+++ view/home.php
@@ -30,11 +30,13 @@ include "./view/header.php";
                 <div class="<?php echo "bg-".$playlist['playlistColor']; ?> w-20 rounded-bl rounded-tl"><div class="flex justify-center items-center text-transparent text-5xl bg-clip-text  bg-gradient-to-tr from-gray-900 to-gray-200 h-20 w-20 ">&#x266B;</div></div>
                 <div class="self-center justify-self-center mx-2">
                     <h2 class="text-gray-50 text-xl"><?php echo $playlist['playlistName']; ?></h2>
-                    <p class="<?php echo "text-".$playlist['playlistColor']; ?>"><?php echo $playlist['username']; ?></p>
+                    <p class="<?php echo "text-".$playlist['playlistColor']; ?> text-md"><?php echo $playlist['username']; ?></p>
+                    <span class=" inline lg:hidden text-gray-50 text-xs"><?php echo "Created at: " .date("d-m-Y", strtotime($playlist['created_at'])) ?></span>
                 </div>
             </div>
-            <div class="flex items-center text-gray-50">
-                <span class="mx-2 hidden lg:inline"><?php echo "Created at: " .date("d-m-Y", strtotime($playlist['created_at'])) ?></span>
+            <div class="flex flex-col justify-center items-center text-gray-50">
+                <span class="mx-2 hidden lg:inline">Created at:</span>
+                <span class="mx-2 hidden lg:inline"><?php echo date("d-m-Y", strtotime($playlist['created_at'])) ?></span>
             </div>
         </div>
     <?php endforeach ?>
